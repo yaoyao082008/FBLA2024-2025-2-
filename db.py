15361c307cfd91cb7e4ce16049ee6089f01b2c6c
yaoyao082008 <94266602+yaoyao082008@users.noreply.github.com>
2025-02-06 21:36:33 -0500

    removed secrets from db and updated readme

--- db.py
+++ db.py
@@ -3,19 +3,6 @@ from firebase_admin import firestore, credentials
 import datetime
 
 # Application Default credentials are automatically created.
-creds = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "fbla-financial",
-  "private_key_id":  "CHANGE THIS PART",
-  "private_key": "CHANGE THIS PART ASWELL",
-  "client_email": "firebase-adminsdk-4x4hb@fbla-financial.iam.gserviceaccount.com",
-  "client_id": "102456473024789919144",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4x4hb%40fbla-financial.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
 app = firebase_admin.initialize_app(creds)
 db = firestore.client()
 
